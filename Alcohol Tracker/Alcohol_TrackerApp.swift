f2aa2ebf879553b79fa0fea77d9deaa8b865dc91
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-10-17 11:48:31 +0100

    Initial Commit

---
+++ Alcohol Tracker/Alcohol_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alcohol_TrackerApp.swift
+//  Alcohol Tracker
+//
+//  Created by Jaiveer Chadda on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Alcohol_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
