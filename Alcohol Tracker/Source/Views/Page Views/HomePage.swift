8202b725e4536b041be7b22458f63d583bd9986b
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2025-10-23 21:29:54 +0100

    updated todo list

--- Alcohol Tracker/Source/Views/Page Views/HomePage.swift
+++ Alcohol Tracker/Source/Views/Page Views/HomePage.swift
@@ -8,24 +8,25 @@
 import SwiftUI
 
 /// To Do:
-/// - colours / dark mode
-///     - change the main background colour
-///     - change the calendar background colour to not be hardcoded
-///         - i.e. it's a transparent background instead
-///     - change the date label text colours (i.e. M, T, W, etc.) so that they either:
-///         - don't turn white when the app is opened on a phone in dark mode
+
+/// ✓ colours / dark mode
+///     ✓ change the main background colour
+///     X change the calendar background colour to not be hardcoded
+///         X i.e. it's a transparent background instead
+///     ✓ change the date label text colours (i.e. M, T, W, etc.) so that they either:
+///         ✓ don't turn white when the app is opened on a phone in dark mode
 ///
-///     - do all of that, OR
-///         - put the app  in perpetual dark mode, so all colours can be hardcoded
-///         - this is a last resort tho tbh - I want it to work in both
-///
+///     X do all of that, OR
+///         X put the app  in perpetual dark mode, so all colours can be hardcoded
+///         X this is a last resort tho tbh - I want it to work in both
+
 /// - functionality
 ///     - basic / set up / clean up
-///         - remove the form from the home page
+///         ✓ remove the form from the home page
 ///         - create a new page with the form on it, which the 'add drink' button leads to
 ///
 ///     - features
-///         - implement basic app functionality with just a pure, design-less form
+///         - implement basic add  functionality with just a pure, design-less form
 ///         - create a very basic page that lists all of the entires the user's entered
 ///
 ///         - then pretty everything up
