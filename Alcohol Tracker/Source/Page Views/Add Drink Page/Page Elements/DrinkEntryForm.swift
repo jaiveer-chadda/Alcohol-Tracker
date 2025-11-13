c1b29f20011d8ab8d6b3ab2b52af689d0bd23584
JV <jaiveer.chadda@gmail.com>
2025-11-13 17:53:01 +0000

    Created basic skeleton of drink type selector

--- Alcohol Tracker/Source/Page Views/Add Drink Page/Page Elements/DrinkEntryForm.swift
+++ Alcohol Tracker/Source/Page Views/Add Drink Page/Page Elements/DrinkEntryForm.swift
@@ -9,6 +9,92 @@ import SwiftUI
 import Foundation
 
 
+
+//let drinkTypeOptions = ["Beer", "Ale", "Mixed Drink", "Shot", "Wine", "Other"]
+
+let ELEMENT_BG_COLOUR: Color = Color(.black).opacity(0.3)
+let SELECTED_BG_COLOUR: Color = Color(.white).opacity(0.1)
+
+let DRINK_TYPE_IMAGES: [String: String] = [
+    "Beer": "mug",
+    "Cider": "cup.and.saucer",
+    "Wine": "wineglass",
+    "Spirits": "drop",
+    "Cocktail": "waterbottle",
+    "Other": "questionmark.app.dashed"
+]
+
+
+struct FormEntryStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .background(ELEMENT_BG_COLOUR)
+            .clipShape(Capsule())
+            .overlay(
+                Capsule()
+                    .stroke(colour.accent.primary, lineWidth:2)
+            )
+    }
+}
+
+
+struct DrinkOption: View {
+    var drinkType: String
+    
+    @Binding var selectedType: String
+    private var isSelected: Bool { selectedType == drinkType }
+    
+    var body: some View {
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .foregroundStyle(isSelected ? SELECTED_BG_COLOUR : ELEMENT_BG_COLOUR)
+            VStack {
+                Image(systemName: DRINK_TYPE_IMAGES[drinkType] ?? "wineglass")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 25)
+                    .padding(.top, 2)
+                Spacer()
+                Text(drinkType)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 2)
+            }
+            .padding()
+        }
+        .frame(height: 100)
+        .onTapGesture { selectedType = drinkType }
+    }
+}
+
+
+struct DrinkTypeSelection: View {
+    @Binding var selectedType: String
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text(" Drink Type")
+                .font(.title2)
+            HStack {
+                DrinkOption(drinkType: "Beer", selectedType: $selectedType)
+                DrinkOption(drinkType: "Cider", selectedType: $selectedType)
+                DrinkOption(drinkType: "Wine", selectedType: $selectedType)
+            }
+            HStack {
+                DrinkOption(drinkType: "Spirits", selectedType: $selectedType)
+                DrinkOption(drinkType: "Cocktail", selectedType: $selectedType)
+                DrinkOption(drinkType: "Other", selectedType: $selectedType)
+            }
+        }
+        .padding(.top)
+    }
+}
+
+
+
 struct DrinkEntryForm: View {
     @Binding var isShowAddDrinkSheet: Bool
     
@@ -18,6 +104,8 @@ struct DrinkEntryForm: View {
     @State private var ABV = ""
     @State private var note = ""
     
+    @State private var selectedType: String = "Other"
+    
     func resetForm() {
         drinkName = ""
         volume = ""
@@ -26,42 +114,60 @@ struct DrinkEntryForm: View {
     }
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Drink Name (optional)", text: $drinkName)
-            }
-            Section {
-                TextField("Volume", text: $volume)
-                    .keyboardType(.numberPad)
-                TextField("ABV", text: $ABV)
-                    .keyboardType(.decimalPad)
-            }
+        
+        VStack {
+            
+            DrinkTypeSelection(selectedType: $selectedType)
+            
+//            Menu {
+//                Picker("Option", selection: $selectedOption) {
+//                    ForEach(drinkTypeOptions, id: \.self) { option in
+//                        Text(option).tag(option)
+//                    }
+//                }
+//            } label: {
+//                Text(selectedOption)
+//                    .multilineTextAlignment(.leading)
+//            }
+            
+            Separator(opacity: 0.25)
+                .padding()
+            
+            
+            TextField("Alcohol Volume", text: $volume)
+                .keyboardType(.numberPad)
+                .textFieldStyle(FormEntryStyle())
+            TextField("ABV", text: $ABV)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(FormEntryStyle())
+            TextField("Drink Name (optional)", text: $drinkName)
+                .textFieldStyle(FormEntryStyle())
             TextField("Note (optional)", text: $note, axis: .vertical)
                 .multilineTextAlignment(strategy: .writingDirectionBased)
                 .frame(height: 100, alignment: .topLeading)
+                .textFieldStyle(FormEntryStyle())
             
-            Section {
-                Button("Save Entry") {
-                    let newEntry = Entry(
-                        id: UUID(),
-                        date: CURRENT_DATE,
-                        
-                        drinkName: drinkName.isEmpty == false ? drinkName : "none_given",
-                        volume: Int(volume) ?? -1,
-                        abv: Double(ABV) ?? -1.0,
-                        note: note.isEmpty == false ? note : "none_given"
-                        
-                    )
-                    dataManager.addEntry(newEntry)
-                    isShowAddDrinkSheet = false
+            
+            Button("Save Entry") {
+                let newEntry = Entry(
+                    id: UUID(),
+                    date: CURRENT_DATE,
                     
-                    self.resetForm()
-                    
-                }
-                .disabled(volume.isEmpty || ABV.isEmpty)
+                    drinkName: drinkName.isEmpty == false ? drinkName : "none_given",
+                    volume: Int(volume) ?? -1,
+                    abv: Double(ABV) ?? -1.0,
+                    note: note.isEmpty == false ? note : "none_given"
+                )
+                dataManager.addEntry(newEntry)
+                isShowAddDrinkSheet = false
+                
+                self.resetForm()
+                
             }
+            .disabled(volume.isEmpty || ABV.isEmpty)
         }
-        .scrollContentBackground(.hidden)
+//        .foregroundStyle(.black)
+        .padding(.horizontal)
              
     }
 }
